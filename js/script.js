fa580f2d6d2fcb07b9de495c88fc31fb1e61b0e3
Klaudia Wlodarczyk <klaudia.wlo990@gmail.com>
2024-07-17 12:20:26 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// console.log("Hello World");
+// anime({
+//     targets: '.img-hero',
+//     scale: {
+//         value: 2,
+//         duration: 1600,
+//         delay: 800,
+//         easing: 'easeInOutQuart'
+//     },
+//     delay: 250 // All properties except 'scale' inherit 250ms delay
+//  });
+ 
